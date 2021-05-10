26dfa946ed54f1822648e6d63f2acf247f0dae1b
Koichi Yoshizaki <kyoshi@oita-u.ac.jp>
2021-05-10 02:49:50 +0000

    Lesson03課題

---
+++ exercise1.rb
@@ -0,0 +1,10 @@
+name = "bundai"
+p "hello world #{name}"
+p 'hello world #{name}'
+
+# +や*の記号もオブジェクト
+name = name + "taro"
+p "hello world #{name}"
+p "hello world #{name*3}"
+
+
---
+++ exercise2.rb
@@ -0,0 +1,5 @@
+# 文字列の先頭及び全ての大文字化
+name = "bundai"
+p "hello world #{name}"
+p "hello world #{name}".capitalize
+p "hello world #{name}".upcase
